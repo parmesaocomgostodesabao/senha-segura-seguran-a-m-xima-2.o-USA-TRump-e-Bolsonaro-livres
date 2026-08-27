f64c0c5af16a6925ddf694ff9bba5ce03ded9072
Parmesão com gosto de sabão <kauan.anastacio.araujo@escola.pr.gov.br>
2026-08-27 08:46:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -36,6 +36,5 @@ function geraSenha(){
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha  + letrasMaiusculas[numeroAleatorio];
     }
+    campoSenha.value = senha
 }
-
-campoSenha.value = letrasMaiusculas
